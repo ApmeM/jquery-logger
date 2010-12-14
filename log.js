cbc303bbdb71d5bd0b777e0a91aea7850ae84b63
ApmeM <artem.votincev@gmail.com>
2010-12-14 21:51:05 +0300

    recursion, opera, error console

--- log.js
+++ log.js
@@ -1,11 +1,34 @@
 (function($){
-	$.logExpand = function(obj, objName){
+
+	var LOG_HTML = 1;
+	var LOG_TEXT = 2;
+	
+	var logExpand = function(obj, objName, logType, visitedObjs){
 		var result = "";
 	    for (var i in obj) {
-			if(typeof obj[i] == 'object')
-				result += $.logExpand(obj[i], objName+'.'+i);
-			else
-				result += "<b>" + objName + "." + i + "</b> = " + obj[i] + "<br />\n";
+			if(typeof obj[i] == 'object'){
+				if(visitedObjs == undefined)
+					visitedObjs = new Array();
+				if($.inArray(obj[i], visitedObjs) == -1){
+					visitedObjs.push(obj[i]);
+					result += logExpand(obj[i], objName + '.' + i, logType, visitedObjs);
+				}else
+				{
+					if(logType == LOG_HTML)
+						result += "<b>" + objName + "." + i + "</b> = " + obj[i] + "<br />";
+					else if(logType == LOG_TEXT)
+						result += objName + "." + i + " = " + obj[i] + "\n";
+					else 
+						throw new Error('Index out of range exception: logType = ', logType);
+				}
+			} else{
+				if(logType == LOG_HTML)
+					result += "<b>" + objName + "." + i + "</b> = " + obj[i] + "<br />";
+				else if(logType == LOG_TEXT)
+					result += objName + "." + i + " = " + obj[i] + "\n";
+				else 
+					throw new Error('Index out of range exception: logType = ', logType);
+			}
 		}
 		return result;
 	}
@@ -14,26 +37,31 @@
 		if(arguments.length == 0) // Nothing to log, everything logged successfully :)
 			return true;
 
-		var args = arguments;
+		// Splice arguments object into array of objects 
+		// (it have a very strange behavior)
+		var args = [].slice.call(arguments);
 		
-		// This is the standard; firebug and newer webkit browsers support this
+		// If we can write into console - we will do it:
+		// firefox (with firebug), chrome and opera know what to do with it
 		if(window.console && window.console.log) {
 			if(window.console.log.apply) {
 				console.log.apply(window.console, args);
 			} else {
 				console.log(args);
 			}
-		}else if(opera) {
-			opera.postError(args); 
 		} else {
-			return false;
+			// Firebug is not installed (or this browser do not support console)
+			// expand object and throw it in separate thread to not stop current operation
+			var expanded = logExpand(args, 'args', LOG_TEXT);
+		    setTimeout(function(){ throw new Error(expanded); }, 0);
 		}
 		return true;
 	};
 
 	$.fn.log = function(obj) {
 		// Going to log into the specified control
-		var args = $.logExpand(obj, 'arguments');
+//		var obj = [].slice.call(arguments);
+		var args = logExpand(obj, 'args', LOG_HTML);
 		this.html(args);
 	}
 })(jQuery);
